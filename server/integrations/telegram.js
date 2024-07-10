cc0fddfd21506ff9d79d97aa065354e3c43ce71c
krafime <krafime@gmail.com>
2024-07-10 14:25:12 +0700

    refactor: Remove unused function and hardcode server location in telegram integration

--- server/integrations/telegram.js
+++ server/integrations/telegram.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
-const { replaceVariables } = require("../util/helpers");
+const moment = require("moment-timezone");
+const {replaceVariables} = require("../util/helpers");
 
 const defaults = {
     finished: "✨ *A speedtest is finished*\n🏓 `Ping`: %ping% ms\n🔼 `Upload`: %upload% Mbps\n🔽 `Download`: %download% Mbps\n📅 `Date`: %currentDate%",
     failed: "❌ *A speedtest has failed*\n`Reason`: %error%\n📅 `Date`: %currentDate%"
-};
+}
 
 const postWebhook = async (token, chatId, message, triggerActivity) => {
     axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
@@ -14,22 +15,11 @@ const postWebhook = async (token, chatId, message, triggerActivity) => {
         .catch(() => triggerActivity(true));
 }
 
-function formatDate(date, offset) {
-    // Create a new date object based on the offset (in minutes)
-    const utc = date.getTime() + (date.getTimezoneOffset() * 6000);
-    const localDate = new Date(utc + (3600000 * offset));
-
-    const year = localDate.getFullYear();
-    const month = String(localDate.getMonth() + 1).padStart(2, '0');
-    const day = String(localDate.getDate()).padStart(2, '0');
-    const hours = String(localDate.getHours()).padStart(2, '0');
-    const minutes = String(localDate.getMinutes()).padStart(2, '0');
-    const seconds = String(localDate.getSeconds()).padStart(2, '0');
-
-    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+function formatDate(date, timezone) {
+    return moment(date).tz(timezone).format("DD-MM-YYYY HH:mm:ss");
 }
 
-const currentDate = formatDate(new Date(), 7); // Set the offset to +7 hours
+const currentDate = formatDate(new Date(), "Asia/Jakarta"); // Menggunakan zona waktu Asia/Jakarta
 
 module.exports = (registerEvent) => {
     registerEvent('testFinished', async (integration, data, activity) => {
@@ -51,12 +41,12 @@ module.exports = (registerEvent) => {
     return {
         icon: "fa-brands fa-telegram",
         fields: [
-            { name: "token", type: "text", required: true, regex: /(\d+):[a-zA-Z0-9_-]+/ },
-            { name: "chat_id", type: "text", required: true, regex: /\d+/ },
-            { name: "send_finished", type: "boolean", required: false },
-            { name: "finished_message", type: "textarea", required: false },
-            { name: "send_failed", type: "boolean", required: false },
-            { name: "error_message", type: "textarea", required: false }
+            {name: "token", type: "text", required: true, regex: /(\d+):[a-zA-Z0-9_-]+/},
+            {name: "chat_id", type: "text", required: true, regex: /\d+/},
+            {name: "send_finished", type: "boolean", required: false},
+            {name: "finished_message", type: "textarea", required: false},
+            {name: "send_failed", type: "boolean", required: false},
+            {name: "error_message", type: "textarea", required: false}
         ]
     };
 }
